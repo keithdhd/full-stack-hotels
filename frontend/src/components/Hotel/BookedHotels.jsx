b0578d8257f5601ca7e92a4dee30b38681eb1230
MariaFrol <maria.frolov@gmail.com>
2025-10-22 11:48:25 +0200

    fsh-6: Change format on BookedHotels component

--- frontend/src/components/Hotel/BookedHotels.jsx
+++ frontend/src/components/Hotel/BookedHotels.jsx
@@ -1,19 +1,33 @@
 function BookedHotels({ hotels }) {
+  const getTotalPrice = () => {
+    return hotels.reduce((acc, hotel) => {
+      return acc + Number(hotel.price);
+    }, 0);
+  };
 
-    const getTotalPrice = () => {
-        return hotels.reduce((acc, hotel) => {
-            return acc + Number(hotel.price);
-        }, 0)
-    }
-
-    return (
-        <>
-            <p>Total price: {getTotalPrice()}</p>
-            {hotels.map(hotel => <p key={hotel.id}>{hotel.name}</p>)}
-        </>
-    )
-
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Hotel Name</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {hotels.map((hotel) => (
+            <tr key={hotel.id}>
+              <td>{hotel.name}</td>
+              <td>{hotel.description}</td>
+              <td>€{hotel.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p>Total price: €{getTotalPrice()}</p>
+    </>
+  );
 }
 
-
-export { BookedHotels }
+export { BookedHotels };
